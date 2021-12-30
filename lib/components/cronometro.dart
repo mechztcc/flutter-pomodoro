d313e18f62dc39fd7a11a7ba4f6b62c3e4349ffb
mechztcc <dojathan@gmail.com>
2021-12-30 14:37:34 -0300

    ajustes store

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -1,63 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pomodoro/components/cronometro_botao.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 class Cronometro extends StatelessWidget {
   const Cronometro({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
     return Container(
       color: Colors.red,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             'Hora de Trabalhar',
             style: TextStyle(
               fontSize: 40,
               color: Colors.white,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Text(
-            '25: 00',
-            style: TextStyle(
+            '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
+            style: const TextStyle(
               fontSize: 120,
               color: Colors.white,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CronometroBotao(
-                  texto: 'Iniciar',
-                  icone: Icons.play_arrow,
+          Observer(
+            builder: (_) => Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (!store.iniciado)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CronometroBotao(
+                      texto: 'Iniciar',
+                      icone: Icons.play_arrow,
+                      click: store.iniciar,
+                    ),
+                  ),
+                if (store.iniciado)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CronometroBotao(
+                      texto: 'Parar',
+                      icone: Icons.stop,
+                      click: store.parar,
+                    ),
+                  ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CronometroBotao(
+                    texto: 'Reiniciar',
+                    icone: Icons.refresh,
+                    click: store.reiniciar,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CronometroBotao(
-                  texto: 'Parar',
-                  icone: Icons.stop,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CronometroBotao(
-                  texto: 'Reiniciar',
-                  icone: Icons.refresh,
-                ),
-              ),
-            ],
-          )
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/components/cronometro_botao.dart
+++ lib/components/cronometro_botao.dart
@@ -4,7 +4,10 @@ class CronometroBotao extends StatelessWidget {
   final String texto;
   final IconData icone;
 
-  const CronometroBotao({Key? key, required this.texto, required this.icone})
+  final void Function()? click;
+
+  const CronometroBotao(
+      {Key? key, required this.texto, required this.icone, this.click})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -12,7 +15,7 @@ class CronometroBotao extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         primary: Colors.black,
       ),
-      onPressed: () {},
+      onPressed: click,
       child: Row(
         children: [
           Padding(
